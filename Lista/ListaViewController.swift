507f23838a438cc27c89a8720aaab439d5427fa8
Kelechi Igbokwe <Kells@kelechis-mbp.local>
2018-01-02 16:14:52 +0100

    Add Table view delegates

--- Lista/ListaViewController.swift
+++ Lista/ListaViewController.swift
@@ -19,6 +19,15 @@ class ListaViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListaItem", for: IndexPath)
+        return cell
+    }
 
 
 }
