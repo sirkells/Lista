7b0078f1c109da38785078288f165b2e9154072b
Kelechi Igbokwe <Kells@kelechis-mbp.local>
2018-01-02 17:34:31 +0100

    Add didselect to toggle checkmark accessory

--- Lista/ListaViewController.swift
+++ Lista/ListaViewController.swift
@@ -47,7 +47,19 @@ class ListaViewController: UITableViewController {
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 
 }
 
